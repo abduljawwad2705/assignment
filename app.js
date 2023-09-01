f98f1a8a7c8220241019b332f6c738c9de077fd0
Abdul jawwad <139391019+abduljawwad2705@users.noreply.github.com>
2023-09-02 00:22:22 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,58 @@
+// CHAPTER # 1
+
+alert("Welcome to my website")
+
+alert("Error!Please enter a valid password")
+
+alert("Welcome to JS world...\nHappy coding!")
+
+alert("Welcome to JS Land")
+
+alert("Happy coding!\n▢ Prevent this page from creating additional dialogs")
+
+alert("hello i can run js through my browser's console")
+
+console.log("hello i can run js through my brower's console")
+
+
+// CHAPTER # 2
+
+var username
+
+var myname = ("Abdul Jawwad")
+
+var message = ("Heello world")
+
+alert (message);
+
+var sname = ("Jhone boe")
+
+alert (sname)
+
+var age = ("15 Years Old")
+
+alert (age)
+
+var result = ("Certified Mobile Application Development")
+
+alert (result)
+
+var pizzars = ("Pizza\nPIZZ\n\Piz\nPiz\nP")
+
+alert (pizzars)
+
+var email = ("abduljawwad675@gmail.com")
+var emailinfo = ("My Email Address")
+
+alert (emailinfo+email)
+
+var book = ("A smarter way to learn javascript")
+var bookinfo = ("i am trying to learn from the book")
+
+alert (bookinfo)
+
+document.write("Yah! I can write HTML content through JAVASCRIPT");
+
+var end =("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+alert ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬") 
